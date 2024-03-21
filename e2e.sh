d60725971d4d8d22011ac075592c905dca00f479
ben2309 <BenRootCI>
2024-03-21 15:58:32 +0000

    add tempo to e2e.sh

--- e2e.sh
+++ e2e.sh
@@ -4,13 +4,15 @@ cd e2e
 
 docker compose down > /dev/null 2>&1 
 
-#sleep 10
+sleep 1
 
 docker compose build
+
+sleep 5 
 docker compose up -d 
-
+sleep 5
 docker compose ps
-
+sleep 10
 docker compose run --rm e2e
 
 docker compose down 
